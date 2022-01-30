5d9d9f1725e9e0aa10c774a995a9d9651e0d5fd4
Hyungjun Seo <floralcustom@gmail.com>
2022-01-30 21:51:26 +0900

    add bcrypt module

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const config = require('./config/key');
 
-const { User } = require("./models/Users");
+const { User } = require("./models/User");
 
 
 
@@ -30,8 +30,7 @@ app.post('/register', (req,res) => {
     //회원가입 할때 필요한 정보들을 Client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
 
-    const user = new User(req.body)
-
+    const user = new User(req.body);
     user.save((err, userInfo) => {
         if(err) return res.json({ success: false, err})
         return res.status(200).json({
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +34,27 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+
+userSchema.pre('save', function( next ){
+
+     var user = this;
+
+     if(user.isModified('password')){
+         //비밀번호를 암호화 시킨다.
+         bcrypt.genSalt(saltRounds, function(err, salt) {
+             if(err) return next(err)
+
+             bcrypt.hash(user.password, salt, function(err, hash) {
+                 // Store hash in your password DB.
+                 if(err) return next(err)
+                 user.password = hash
+                 next();
+             });
+         });
+     }
+})
+
 
 const User = mongoose.model('User', userSchema)
 
