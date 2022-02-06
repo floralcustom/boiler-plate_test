ac8215b346784a185a9898932f79cb1e342ad926
Hyungjun Seo <floralcustom@gmail.com>
2022-02-06 20:44:39 +0900

    test

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ function LandingPage() {
 
   
   useEffect(()=>{
-    axios.get('/api/hello')
+    axios.get('http://localhost:5000/api/hello')
     .then(response => console.log(response.data))
   }, [])
   
